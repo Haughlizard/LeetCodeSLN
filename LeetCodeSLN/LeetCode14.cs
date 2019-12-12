358c2a9c410799fec2951a7611813da815c54ce0
生延 李 <lishengyan1991@qq.com>
2019-12-12 08:54:42 +0800

    问题14解答提交

---
+++ LeetCodeSLN/LeetCode14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSLN
+{
+    public class LeetCode14
+    {
+        /// <summary>
+        /// 查找最长公共前缀
+        /// </summary>
+        /// <param name="strs"></param>
+        /// <returns></returns>
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0 || strs[0] == "") return "";
+            for(int i = 0; i < strs[0].Length; i++)
+            {
+                for(int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j] == "") return "";
+
+                    if (i < strs[j].Length)
+                    {
+                        if (strs[j][i] != strs[0][i])
+                        {
+                            return strs[0].Substring(0, i);
+                        }
+                    }else
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+            }
+            return strs[0];
+        }
+    }
+}
---
+++ LeetCodeUnitTest/LeetCode14UnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LeetCodeSLN;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeUnitTest
+{
+    [TestClass]
+    public class LeetCode14UnitTest
+    {
+        [TestMethod]
+        public void LeetCode14TestMethod1()
+        {
+            string[] strings = new string[3] { "flight","flower","flow"};
+            var prefix = LeetCode14.LongestCommonPrefix(strings);
+            Assert.AreEqual("fl", prefix);
+        }
+
+        [TestMethod]
+        public void LeetCode14TestMethod2()
+        {
+            string[] strings = new string[3] { "dog", "racecar", "car" };
+            var prefix = LeetCode14.LongestCommonPrefix(strings);
+            Assert.AreEqual("", prefix);
+        }
+
+        [TestMethod]
+        public void LeetCode14TestMethod3()
+        {
+            string[] strings = new string[2] { "aa", "a"};
+            var prefix = LeetCode14.LongestCommonPrefix(strings);
+            Assert.AreEqual("a", prefix);
+        }
+    }
+}
